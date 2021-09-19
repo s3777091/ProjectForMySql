0876779c27cbfcd1f6260a5395ce854025aaf4c8
s3777091 <s3777091@rmit.edu.vn>
2021-09-19 13:44:25 +0700

    ad

--- public/js/Orderschart.js
+++ public/js/Orderschart.js
@@ -37,7 +37,7 @@ async function getData() {
   // tạo biến private
   // mở crt + F11 mở cửa sổ console sẽ thấy
   // gọi giá trị stock là cái nhập vào đẻ filter nhưng ko hiểu sao ko vào
-  const response = await fetch("http://localhost:9999/admin/api/order1000");
+  const response = await fetch("https://tradesql.herokuapp.com/admin/api/order1000");
 
   // sử dụng api từ web
   const data = await response.json();
